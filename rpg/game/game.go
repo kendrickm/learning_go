84f1781f020a8be20835f97317becda70fde5878
Kendrick <kmartinix@gmail.com>
2024-02-15 20:58:03 -0800

    Finished episode 23

--- rpg/game/game.go
+++ rpg/game/game.go
@@ -2,11 +2,20 @@ package game
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 type GameUI interface {
-	Draw(*Level)
+	DrawThenGetInput(*Level) Input
+}
+
+type Input struct {
+	up    bool
+	down  bool
+	left  bool
+	right bool
+	ESC   bool
 }
 
 type Tile rune
@@ -15,16 +24,34 @@ const (
 	StoneWall Tile = '#'
 	DirtFloor Tile = '.'
 	Door      Tile = '|'
-	Blank     Tile = ' '
+	Blank     Tile = 0
+	Pending   Tile = -1
 )
 
+type Entity struct {
+	X, Y int
+}
+
+type Player struct {
+	Entity
+}
+
 type Level struct {
-	Map [][]Tile
+	Map    [][]Tile
+	Player Player
 }
 
 func Run(ui GameUI) {
 	level := loadLevelFromFile("game/maps/level1.map")
-	ui.Draw(level)
+
+	for {
+		input := ui.DrawThenGetInput(level)
+		fmt.Println(input)
+		if input.ESC {
+			break
+		}
+	}
+
 }
 
 func loadLevelFromFile(filename string) *Level {
@@ -39,7 +66,7 @@ func loadLevelFromFile(filename string) *Level {
 	longestRow := 0
 	index := 0
 	for scanner.Scan() {
-		//fmt.Println(scanner.Text())
+		// fmt.Println(scanner.Text())
 		levelLines = append(levelLines, scanner.Text())
 		if len(levelLines[index]) > longestRow {
 			longestRow = len(levelLines[index])
@@ -55,23 +82,46 @@ func loadLevelFromFile(filename string) *Level {
 
 	for y := 0; y < len(level.Map); y++ {
 		line := levelLines[y]
-		for x := 0; x < longestRow; x++ {
-			for _, c := range line {
-				var t Tile
-				switch c {
-				case ' ', '\n', '\t', '\r':
-					t = Blank
-				case '#':
-					t = StoneWall
-				case '|':
-					t = Door
-				case '.':
-					t = DirtFloor
-				default:
-					panic("Invalid character in map")
-				}
-				level.Map[y][x] = t
 
+		for x, c := range line {
+			var t Tile
+
+			switch c {
+			case ' ', '\n', '\t', '\r':
+				t = Blank
+			case '#':
+				t = StoneWall
+			case '|':
+				t = Door
+			case '.':
+				t = DirtFloor
+			case 'P':
+				level.Player.X = x
+				level.Player.Y = y
+				t = Pending
+			default:
+				panic("Invalid character in map")
+			}
+			level.Map[y][x] = t
+
+		}
+	}
+
+	for y, row := range level.Map {
+		for x, tile := range row {
+			if tile == Pending {
+			SearchLoop:
+				for searchX := x - 1; searchX <= x+1; searchX++ {
+					for searchY := y - 1; searchY <= y+1; searchY++ {
+						searchTile := level.Map[searchY][searchX]
+						switch searchTile {
+						case DirtFloor:
+							level.Map[y][x] = DirtFloor
+							break SearchLoop
+						}
+
+					}
+				}
 			}
 		}
 	}
--- rpg/ui2d/ui2d.go
+++ rpg/ui2d/ui2d.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -19,9 +20,10 @@ const winWidth, winHeight = 1280, 720
 
 var renderer *sdl.Renderer
 var textureAtlas *sdl.Texture
-var textureIndex map[game.Tile]sdl.Rect
+var textureIndex map[game.Tile][]sdl.Rect
 
 func loadTextureIndex() {
+	textureIndex = make(map[game.Tile][]sdl.Rect)
 	file, err := os.Open("ui2d/assets/asset-index.txt")
 	if err != nil {
 		panic(err)
@@ -32,20 +34,37 @@ func loadTextureIndex() {
 		line = strings.TrimSpace(line)
 		tile := game.Tile(line[0])
 		xy := line[1:]
-		splitXy := strings.Split(xy, ",")
-		x, err := strconv.ParseInt(strings.TrimSpace(splitXy[0]), 10, 64)
+		splitXYCount := strings.Split(xy, ",")
+		x, err := strconv.ParseInt(strings.TrimSpace(splitXYCount[0]), 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		y, err := strconv.ParseInt(strings.TrimSpace(splitXy[1]), 10, 64)
+		y, err := strconv.ParseInt(strings.TrimSpace(splitXYCount[1]), 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(tile, x, y)
+		variationCount, err := strconv.ParseInt(strings.TrimSpace(splitXYCount[2]), 10, 64) //Supports randomly picking from a batch of tiles
+		if err != nil {
+			panic(err)
+		}
+		var rects []sdl.Rect
+		for i := int64(0); i < variationCount; i++ {
+			rects = append(rects, sdl.Rect{int32(x * 32), int32(y * 32), 32, 32})
+			x++
+			if x > 62 { //handles wrap arounds
+				x = 0
+				y++
+			}
+
+		}
+
+		textureIndex[tile] = rects
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 func imgFileToTexture(filename string) *sdl.Texture {
@@ -98,19 +117,31 @@ func init() {
 type UI2d struct {
 }
 
-func (*UI2d) Draw(level *game.Level) {
-	fmt.Println("We did something")
-
-	renderer.Copy(textureAtlas, nil, nil)
+func (*UI2d) DrawThenGetInput(level *game.Level) game.Input {
+	i := game.Input{}
+	rand.Seed(2)
+	for y, row := range level.Map {
+		for x, tile := range row {
+			if tile != game.Blank {
+				srcRects := textureIndex[tile]
+				srcRect := srcRects[rand.Intn(len(srcRects))]
+				dstRect := sdl.Rect{int32(x) * 32, int32(y) * 32, 32, 32}
+				renderer.Copy(textureAtlas, &srcRect, &dstRect)
+			}
+			//fmt.Println(tile)
+		}
+	}
+	renderer.Copy(textureAtlas, &sdl.Rect{21 * 32, 59 * 32, 32, 32}, &sdl.Rect{int32(level.Player.X) * 32, int32(level.Player.Y) * 32, 32, 32})
 	renderer.Present()
-   for {
-	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-	
-	  switch  event.(type) {
-          case *sdl.QuitEvent:
-             return
-          }
-        }
-    }
-	sdl.Delay(3000)
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				i.ESC = true
+				return i
+			}
+		}
+	}
+	return i
 }
