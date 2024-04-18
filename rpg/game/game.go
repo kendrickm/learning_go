d7f06092e4e4e2aec774c305f7e03c4024a52b68
Kendrick <kmartinix@gmail.com>
2024-04-18 12:12:58 -0700

    Fixed input handling

--- rpg/game/game.go
+++ rpg/game/game.go
@@ -73,10 +73,81 @@ type Level struct {
 	Debug  map[Pos]bool
 }
 
-// func (p priorityArray) Len() int           { return len(p) }
-// func (p priorityArray) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-// func (p priorityArray) Less(i, j int) bool { return p[i].priority < p[j].priority }
+func loadLevelFromFile(filename string) *Level {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+	levelLines := make([]string, 0)
+	longestRow := 0
+	index := 0
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+		levelLines = append(levelLines, scanner.Text())
+		if len(levelLines[index]) > longestRow {
+			longestRow = len(levelLines[index])
+		}
+		index++
+	}
+
+	level := &Level{}
+	level.Map = make([][]Tile, len(levelLines))
+	for i := range level.Map {
+		level.Map[i] = make([]Tile, longestRow)
+	}
+
+	for y := 0; y < len(level.Map); y++ {
+		line := levelLines[y]
+
+		for x, c := range line {
+			var t Tile
+
+			switch c {
+			case ' ', '\n', '\t', '\r':
+				t = Blank
+			case '#':
+				t = StoneWall
+			case '|':
+				t = ClosedDoor
+			case '/':
+				t = OpenDoor
+			case '.':
+				t = DirtFloor
+			case 'P':
+				level.Player.X = x
+				level.Player.Y = y
+				t = Pending
+			default:
+				panic("Invalid character in map")
+			}
+			level.Map[y][x] = t
+
+		}
+	}
+
+	for y, row := range level.Map {
+		for x, tile := range row {
+			if tile == Pending {
+			SearchLoop:
+				for searchX := x - 1; searchX <= x+1; searchX++ {
+					for searchY := y - 1; searchY <= y+1; searchY++ {
+						searchTile := level.Map[searchY][searchX]
+						switch searchTile {
+						case DirtFloor:
+							level.Map[y][x] = DirtFloor
+							break SearchLoop
+						}
+
+					}
+				}
+			}
+		}
+	}
+	return level
+}
 func canWalk(level *Level, pos Pos) bool {
 	t := level.Map[pos.Y][pos.X]
 
@@ -267,82 +338,11 @@ func (game *Game) Run() {
 		if len(game.LevelChans) == 0 {
 			return
 		}
-	}
 
-}
-
-func loadLevelFromFile(filename string) *Level {
-	file, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	levelLines := make([]string, 0)
-	longestRow := 0
-	index := 0
-	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
-		levelLines = append(levelLines, scanner.Text())
-		if len(levelLines[index]) > longestRow {
-			longestRow = len(levelLines[index])
-		}
-		index++
-	}
-
-	level := &Level{}
-	level.Map = make([][]Tile, len(levelLines))
-	for i := range level.Map {
-		level.Map[i] = make([]Tile, longestRow)
-	}
-
-	for y := 0; y < len(level.Map); y++ {
-		line := levelLines[y]
-
-		for x, c := range line {
-			var t Tile
-
-			switch c {
-			case ' ', '\n', '\t', '\r':
-				t = Blank
-			case '#':
-				t = StoneWall
-			case '|':
-				t = ClosedDoor
-			case '/':
-				t = OpenDoor
-			case '.':
-				t = DirtFloor
-			case 'P':
-				level.Player.X = x
-				level.Player.Y = y
-				t = Pending
-			default:
-				panic("Invalid character in map")
-			}
-			level.Map[y][x] = t
-
+		// Send game state updates
+		for _, lchan := range game.LevelChans {
+			lchan <- game.Level
 		}
 	}
 
-	for y, row := range level.Map {
-		for x, tile := range row {
-			if tile == Pending {
-			SearchLoop:
-				for searchX := x - 1; searchX <= x+1; searchX++ {
-					for searchY := y - 1; searchY <= y+1; searchY++ {
-						searchTile := level.Map[searchY][searchX]
-						switch searchTile {
-						case DirtFloor:
-							level.Map[y][x] = DirtFloor
-							break SearchLoop
-						}
-
-					}
-				}
-			}
-		}
-	}
-	return level
 }
